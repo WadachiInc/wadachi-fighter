6eff5692bfe073c7c21108a321e2157e679d739b
スィーリー　ネイソン <n.seeley@wdch.jp>
2015-02-26 12:22:54 +0900

    ニュースページのindexルートを追加しました
    前の30日からのコミットはgithubのから引き出されています

---
+++ app/routes/news/index.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+  // ---------------------------------- メソッド
+
+  // データモデルを検索する
+  model: function(/*params, transition, queryParams*/) {
+    var date = new Date();
+    date.setDate(date.getDate() - 30);
+    return this.get("store").find("commit", { since: date });
+  }
+});
